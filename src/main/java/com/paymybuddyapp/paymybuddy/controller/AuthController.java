c72d391b946c454d03e57692175e85cc36eaefc3
Michelle Mairet <michelle.rk.mairet@gmail.com>
2024-07-31 12:05:13 +0200

    Corrections of auth and home controller, adding data in data.sql for tests

--- src/main/java/com/paymybuddyapp/paymybuddy/controller/AuthController.java
+++ src/main/java/com/paymybuddyapp/paymybuddy/controller/AuthController.java
@@ -1,10 +1,24 @@
 package com.paymybuddyapp.paymybuddy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.paymybuddyapp.paymybuddy.model.User;
+import com.paymybuddyapp.paymybuddy.service.UserService;
 
 @Controller
 public class AuthController {
+
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@GetMapping("/login")
 	public String login() {
@@ -14,5 +28,18 @@ public class AuthController {
 	@GetMapping("/register")
 	public String register() {
 		return "register";
+	}
+
+	@PostMapping("/register")
+	public String processRegistration(@RequestParam String username, @RequestParam String email,
+			@RequestParam String password, Model model) {
+
+		User user = new User();
+		user.setUsername(username);
+		user.setEmail(email);
+		user.setPassword(passwordEncoder.encode(password));
+		userService.saveUser(user);
+
+		return "redirect:/login";
 	}
 }
--- src/main/java/com/paymybuddyapp/paymybuddy/controller/HomeController.java
+++ src/main/java/com/paymybuddyapp/paymybuddy/controller/HomeController.java
@@ -1,10 +1,22 @@
 package com.paymybuddyapp.paymybuddy.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.paymybuddyapp.paymybuddy.model.User;
+import com.paymybuddyapp.paymybuddy.service.UserService;
 
 @Controller
 public class HomeController {
+
+	@Autowired
+	private UserService userService;
 
 	@GetMapping("/")
 	public String home() {
@@ -12,8 +24,21 @@ public class HomeController {
 	}
 
 	@GetMapping("/profile")
-	public String profile() {
-		return "profile";
+	public String profile(Model model) {
+		// Récupération de l'utilisateur actuellement authentifié
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username = authentication.getName();
+
+		// Recherche de l'utilisateur dans la BDD
+		Optional<User> currentUserOptional = userService.getUserByUsername(username);
+
+		if (currentUserOptional.isPresent()) {
+			User currentUser = currentUserOptional.get();
+			model.addAttribute("user", currentUser);
+			return "profile";
+		} else {
+			return "redirect:/login?error=usernotfound";
+		}
 	}
 
 	@GetMapping("/add-connection")
